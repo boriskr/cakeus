b2862a6622052f4bf1981d2b7b92334adbe8b0ad
Boris Kraportov <boris@iloveboris.com>
2011-11-05 03:12:29 +0400

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cakeus::Application.config.secret_token = '9385e00f42a9be87e0ea2aa8b2ff10c5c5fcc2d56b53cdc70dd936fe05eb2e99b83222e427c493727018ae2d5b61bece4eb88a2761713a794a6d90126d99f3f5'
